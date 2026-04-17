f89e0bd3df486daaadcb1114a7f1691a0c8571c9
mateoescobarj16-svg <mateoescobarj.16@gmail.com>
2026-04-17 14:03:29 -0300

    Subida inicial app Spartans

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spartans App Final
+
+## Ejecutar local
+```bash
+npm install
+npm run dev
+```
+
+## Build
+```bash
+npm run build
+```
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SpartansPagosApp from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SpartansPagosApp />
+  </React.StrictMode>
+);
